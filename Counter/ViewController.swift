a92085de638ddefe56867b9fa15266d7a00fa9fa
dsiberia9s <dsiberia9s@icloud.com>
2023-04-15 21:23:09 +0700

    Добавлен весь необходимый функционал согласно ТЗ усложнённой задачи.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,9 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var counter: Int = 0
+    
+    @IBOutlet weak var historyUITextView: UITextView!
+    @IBOutlet weak var countUILabel: UILabel!
+    
+    @IBAction func plusUIButton(_ sender: Any) {
+        counter += 1
+        log(message: "значение изменено на +1")
+    }
+    
+    @IBAction func minusUIButton(_ sender: Any) {
+        if counter > 0 {
+            counter -= 1
+            log(message: "значение изменено на -1")
+        } else {
+            log(message: "попытка уменьшить значение счётчика ниже 0")
+        }
+    }
+    
+    @IBAction func resetUIButton(_ sender: Any) {
+        counter = 0
+        log(message: "значение сброшено")
+    }
+    
+    func log(message: String) {
+        let date = Date()
+        let format = DateFormatter()
+        format.dateStyle = .short
+        format.timeStyle = .medium
+        
+        countUILabel.text = "Значение счётчика: \(counter)"
+        
+        historyUITextView.text += "[\(format.string(from: date))]: \(message).\n"
+        
+        let range = NSMakeRange(historyUITextView.text.count - 1, 0)
+        historyUITextView.scrollRangeToVisible(range)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.        
+        historyUITextView.text += "\n"
+        
         print("Welcome!")
     }
 
